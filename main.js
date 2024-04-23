92d0da13841e6a43c8e2f6c318f106baddda9f28
aymenthedeveloper <aymendev.contact@gmail.com>
2024-04-23 15:00:57 +0100

    final adjustment

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-let time = 38438;
+let today = Date.now();
+let time = Math.trunc(today / 1000) % (3600 * 24) + 3600;
 let hh1, hh2, mm1, mm2, ss1, ss2, timeLeft;
-function createClockNumbers(elementsContainer, elementsNumber, elementsClass){
+function createClockNumbers(elementsContainer, elementsNumber){
+  elementsContainer.style.setProperty('--x', 0);
   for (let i = 0; i <= elementsNumber; i++) {
-    const n = document.createElement('div');
-    n.classList.add(elementsClass)
+    const n = document.createElement('span');
+    n.classList.add('number')
     if (i === 0 ) {n.classList.add('current')}
     n.innerText = i;
     elementsContainer.appendChild(n)
   }
 }
-
 
 function getCurrentNumber(elementsContainer, prevNumber, currentNumber, currentIndex){
   prevNumber.classList.remove('current');
@@ -19,39 +20,36 @@ function getCurrentNumber(elementsContainer, prevNumber, currentNumber, currentI
 }
 
 const hours1 = document.querySelector('.hours1');
-createClockNumbers(hours1, 2, 'h1')
-const h1 = document.querySelectorAll('.h1');
-let prevHoursNumber1 = h1[0];
-
 const hours2 = document.querySelector('.hours2');
-createClockNumbers(hours2, 9, 'h2')
-const h2 = document.querySelectorAll('.h2');
-let prevHoursNumber2 = h2[0];
-
 const minutes1 = document.querySelector('.minutes1');
-createClockNumbers(minutes1, 5, 'm1')
-const m1 = document.querySelectorAll('.m1');
-let prevMinutesNumber1 = m1[0];
-
 const minutes2 = document.querySelector('.minutes2');
-createClockNumbers(minutes2, 9, 'm2')
-const m2 = document.querySelectorAll('.m2');
-let prevMinutesNumber2 = m2[0];
-
-const seconds1 = document.querySelector('.seconds1');
-createClockNumbers(seconds1, 5, 's1')
-const s1 = document.querySelectorAll('.s1');
-let prevDigit1 = s1[0];
-
 const seconds2 = document.querySelector('.seconds2');
-createClockNumbers(seconds2, 9, 's2')
-const s2 = document.querySelectorAll('.s2');
-let prevDigit2 = s2[0];
+const seconds1 = document.querySelector('.seconds1');
+
+createClockNumbers(hours1, 2)
+createClockNumbers(hours2, 9)
+createClockNumbers(minutes1, 5)
+createClockNumbers(minutes2, 9)
+createClockNumbers(seconds1, 5)
+createClockNumbers(seconds2, 9)
+
+const h1 = hours1.children;
+const h2 = hours2.children;
+const m1 = minutes1.children;
+const m2 = minutes2.children;
+const s1 = seconds1.children;
+const s2 = seconds2.children;
+
+let [prevHoursNumber1] = h1;
+let [prevHoursNumber2] = h2;
+let [prevMinutesNumber1] = m1;
+let [prevMinutesNumber2] = m2;
+let [prevSecondsNumber1] = s1;
+let [prevSecondsNumber2] = s2;
 
 
 setInterval(() => {
   time++
-  if (time >= 3600 * 24) {time = 0;}
   hh1 = Math.floor(time / 36000) % 3;
   timeLeft = time % 36000;
   prevHoursNumber1 = getCurrentNumber(hours1, prevHoursNumber1, h1[hh1], hh1);
@@ -65,8 +63,7 @@ setInterval(() => {
   prevMinutesNumber2 = getCurrentNumber(minutes2, prevMinutesNumber2, m2[mm2], mm2);
   timeLeft = timeLeft % 60;
   ss1 = Math.trunc(timeLeft / 10) % 6;
-  prevDigit1 = getCurrentNumber(seconds1, prevDigit1, s1[ss1], ss1);
+  prevSecondsNumber1 = getCurrentNumber(seconds1, prevSecondsNumber1, s1[ss1], ss1);
   ss2 = timeLeft % 10;
-  prevDigit2 = getCurrentNumber(seconds2, prevDigit2, s2[ss2], ss2);
+  prevSecondsNumber2 = getCurrentNumber(seconds2, prevSecondsNumber2, s2[ss2], ss2);
 },1000)
-
